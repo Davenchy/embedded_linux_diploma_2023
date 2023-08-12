b8860ef44264372710319d7e90be823fbbe3bc5c
Fadi Asaad <firon1222@gmail.com>
2023-08-12 08:19:21 +0300

    update `is_vowel_task2.py` in `python:01` and add some doctests

--- 01_python/01_introduction/is_vowel_task2.py
+++ 01_python/01_introduction/is_vowel_task2.py
@@ -2,15 +2,50 @@
 # check if letter is a vowel
 
 def is_vowel(letter):
-    return letter.lower() in ['a', 'e', 'i', 'o', 'u']
+    """Check if letter is a vowel
+
+    Args:
+        letter (str): the letter to check
+
+    Returns: bool
+
+    Examples:
+
+        >>> is_vowel('a')
+        True
+
+        >>> is_vowel('b')
+        False
+
+        >>> is_vowel('A')
+        True
+
+        >>> is_vowel(50)
+        Traceback (most recent call last):
+        TypeError: Letter must be a string
+
+        >>> is_vowel(True)
+        Traceback (most recent call last):
+        TypeError: Letter must be a string
+
+        >>> is_vowel('')
+        Traceback (most recent call last):
+        ValueError: Letter must be at least one character
+        """
+    if type(letter) is not str:
+        raise TypeError("Letter must be a string")
+    if len(letter) < 1:
+        raise ValueError("Letter must be at least one character")
+    return letter[0].lower() in ['a', 'e', 'i', 'o', 'u']
 
 
-while 1:
-    letter = input("> Enter a letter [nothing to quit]: ")
-    if not letter:
-        break
+if __name__ == '__main__':
+    while 1:
+        letter = input("> Enter a letter [nothing to quit]: ")
+        if not letter:
+            break
 
-    if is_vowel(letter[0]):
-        print("Vowel")
-    else:
-        print("Consonant")
+        if is_vowel(letter):
+            print("Vowel")
+        else:
+            print("Consonant")
--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Embedded Linux Diploma 23 - Tasks
 
 - This repo contains my tasks solution for the diploma
+
+## Tests
+
+- Some tasks have __doctests__
+To run doctests use `python3 -m doctest`
+
+```bash
+python3 -m doctest my_script.py
+```
+
