50b630327bd2a83ca2a52ca59e5eb8e3959c554d
Fadi Asaad <firon1222@gmail.com>
2023-09-13 00:08:23 +0300

    update right angled triangle cpp task

--- 02_cpp/is_right_trig.cpp
+++ 02_cpp/is_right_trig.cpp
@@ -3,32 +3,39 @@
 #include <iostream>
 #include <vector>
 
+using std::cout, std::cin, std::endl;
+using std::pow;
+using std::vector, std::max_element;
+
 int main() {
   int a, b, c, hyp;
 
-  std::cout << "Enter three sides of a triangle:-" << std::endl;
-  std::cout << "Side A: ";
-  std::cin >> a;
-  std::cout << "Side B: ";
-  std::cin >> b;
-  std::cout << "Side C: ";
-  std::cin >> c;
+  cout << "Enter three sides of a triangle:-" << endl;
+  cout << "Side A: ";
+  cin >> a;
+  cout << "Side B: ";
+  cin >> b;
+  cout << "Side C: ";
+  cin >> c;
 
-  std::vector<int> sides = {a, b, c};
+  vector<int> sides = {a, b, c};
+
+  // raise all sides to the power of 2
+  for (auto &side : sides)
+    side = pow(side, 2);
 
   // get the largest side as hypotenuse
-  auto max_e = std::max_element(sides.begin(), sides.end());
+  auto max_e = max_element(sides.begin(), sides.end());
   hyp = *max_e;
   sides.erase(max_e);
 
   // check if it is a right angled triangle
-  bool is_right =
-      std::pow(sides[0], 2) + std::pow(sides[1], 2) == std::pow(hyp, 2);
+  bool is_right = sides[0] + sides[1] == hyp;
 
   if (is_right)
-    std::cout << "This is a right angled triangle." << std::endl;
+    cout << "This is a right angled triangle." << endl;
   else
-    std::cout << "This is not a right angled triangle." << std::endl;
+    cout << "This is not a right angled triangle." << endl;
 
   return 0;
 }
