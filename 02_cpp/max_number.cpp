b55c3fb01c93be41e8d01791bb2f910cd2faa8bf
Fadi Asaad <firon1222@gmail.com>
2023-09-12 22:21:28 +0300

    add comments to max_number.cpp

--- 02_cpp/max_number.cpp
+++ 02_cpp/max_number.cpp
@@ -8,17 +8,22 @@ int main() {
   std::string user_input;
 
   std::cout << "Enter numbers separated by spaces: ";
+  // read single line from user
   std::getline(std::cin, user_input);
 
+  // create string stream from the user input
   std::stringstream input_stream(user_input);
   std::string word;
 
+  // create regex to validate input
   std::regex reg("^[0-9]{1,10}$");
   while (input_stream >> word) {
     if (!std::regex_match(word, reg))
       continue;
 
+    // convert string to int
     int value = std::stoi(word);
+    // if value is greater than max then replace max with value
     if (value > max)
       max = value;
   }
